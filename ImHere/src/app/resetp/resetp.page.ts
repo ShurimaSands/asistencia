8b79dc7c1a49bcb844ebb9304c0e859a150d3ee7
Camilo.T.Barraza <cam.trivino@gmail.com>
2023-08-29 12:05:40 -0400

    scroll "arreglado" y flecha para atras

--- ImHere/src/app/resetp/resetp.page.ts
+++ ImHere/src/app/resetp/resetp.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-resetp',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetpPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  goBack(){
+    this.router.navigate(['/login'])
   }
 
 }
